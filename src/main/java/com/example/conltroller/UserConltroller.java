63c4b8bd0f6ee0d80c125b03b769b3ab671f8918
siao <siaojing@qq.com>
2021-12-25 13:11:29 +0800

    Initial commit

---
+++ src/main/java/com/example/conltroller/UserConltroller.java
@@ -0,0 +1,10 @@
+package com.example.conltroller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/user")
+public class UserConltroller {
+
+
+
+}
---
+++ src/main/java/com/example/pojo/User.java
@@ -0,0 +1,10 @@
+package com.example.pojo;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String name;
+    private Integer age;
+    private String address;
+}
